8371a2117ae40a3fccae457b60752b9be148f930
lucas44993 <lucasabraao2020@hotmail.com>
2025-08-01 18:47:35 -0300

    feat(backend,compose): adicionei a pasta do backend e o compose da aplicação

---
+++ backend/src/main/java/com/tchau/message/TchauMessageApplication.java
@@ -0,0 +1,13 @@
+package com.tchau.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TchauMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TchauMessageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tchau/message/TchauMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchau.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TchauMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
