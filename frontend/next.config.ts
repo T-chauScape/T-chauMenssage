ede268bc58309f2029cd16b8922862b748b7b909
vVuc <tmenezessilva34@gmail.com>
2025-08-01 22:43:17 -0300

    feat(compose,next.config): hot-Reload implementado no frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000, // Verifica a cada 1000ms
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+}
 
 export default nextConfig;
